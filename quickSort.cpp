de6886f2b0ec613d1847f0f3ff15ac758f762704
akuma-dhruv <dhruvgarg79@gmail.com>
2020-05-30 22:26:56 +0530

    Quick sort

---
+++ quickSort.cpp
@@ -0,0 +1,53 @@
+#include<iostream>
+using namespace std;
+int partition(int arr[], int low, int high)
+{
+    int pivot = arr[high];    // pivot
+    int i = (low - 1);  // Index of smaller element
+
+    for (int j = low; j <= high- 1; j++)
+    {
+        // If current element is smaller than or
+        // equal to pivot
+        if (arr[j] <= pivot)
+        {
+            i++;    // increment index of smaller element
+            swap(arr[i], arr[j]);
+        }
+    }
+    swap(arr[i + 1], arr[high]);
+    return (i + 1);
+}
+ int quicksort(int arr[],int si,int ei)
+ {
+
+
+     int c;
+     if(si>=ei|| ei==0)
+    {
+
+        return 0;
+    }
+    c=partition(arr,si,ei);
+
+    quicksort(arr,si,c-1);
+    quicksort(arr,c+1,ei);
+
+ }
+int main()
+{
+    int s=7,n=0;
+
+int arr[]={5,9,3,2,1,18,6};
+
+
+
+
+quicksort(arr,0,s-1);
+n=0;
+while(n<s)
+{
+cout<<arr[n]<<" ";
+n++;
+}
+}
